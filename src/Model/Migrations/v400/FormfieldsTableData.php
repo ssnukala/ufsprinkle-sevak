c30351006f96eee1181130163792548de423f7c1
Srinivas N <me@srinivasnukala.com>
2017-05-28 08:29:53 -0500

    Updated Namespace for sndbforms

--- src/Model/Migrations/v400/FormfieldsTableData.php
+++ src/Model/Migrations/v400/FormfieldsTableData.php
@@ -13,7 +13,7 @@ namespace UserFrosting\Sprinkle\Sevak\Model\Migrations\v400;
 use UserFrosting\System\Bakery\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
-use UserFrosting\Sprinkle\Sndbforms\Model\Formfields;
+use UserFrosting\Sprinkle\SnDbForms\Model\Formfields;
 
 /**
  * Formfields table migration
